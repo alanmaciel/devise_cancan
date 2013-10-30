bea91d095d7f0b4e0ae295d028f362f234318154
alan m <alan@ubernetics.com>
2013-10-30 12:27:36 -0600

    A journey of a thousand miles begins with a single step.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanc::Application.config.secret_token = 'e7d59d033b98ad72b1118ecfd3aad07688b31882750b7d3009271e5e8e2e0926230c28e691ca817ee9775df733d73b41898581dca67ba9df97d2aa663cbf7890'
