53cfd100b72b577caee3f1d18f786110df2d19f2
alan m <alan@ubernetics.com>
2013-10-30 19:38:46 -0600

    Administrative page 2 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,9 @@ YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name({ :name => role }, :without_protection => true)
   puts 'role: ' << role 
 end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, 
-                                    :email => ENV['ADMIN_EMAIL'].dup, 
-                                    :password => ENV['ADMIN_PASSWORD'].dup, 
-                                    :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'ADDING DEFAULT USERS...'
+user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+
 puts 'user: ' << user.name
 user.add_role :admin
 user2 = User.find_or_create_by_email  :name => 'Second User', 
