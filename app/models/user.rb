613092b03be6ad37edc523a4b8e2f214a175ddbc
alan m <alan@ubernetics.com>
2013-10-30 20:06:43 -0600

    Assign a default role to new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ActiveRecord::Base
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  before_create :assign_role
+
   validates_presence_of :name
   validates_uniqueness_of :email, :case_sensitive => false
+
+  def assign_role
+    # assign a default role if no role is assigned
+    self.add_role :user if self.roles.first.nil?
+  end
+
 end
